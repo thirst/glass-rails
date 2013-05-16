7d4f0925a615bc9b8efa4c56d922edbb2d0b51ca
Han <hkfortytwo@gmail.com>
2013-05-16 16:12:49 -0700

    add id_token to google account

--- lib/generators/glass/install/install_generator.rb
+++ lib/generators/glass/install/install_generator.rb
@@ -15,7 +15,7 @@ module Glass
         Time.now.utc.strftime("%Y%m%d%H%M%S")
       end
       def create_glass_account_migration
-        generate("model", "google_account token refresh_token expires_at:integer email name #{user_model.underscore.singularize}:references")
+        generate("model", "google_account token refresh_token expires_at:integer email name id_token #{user_model.underscore.singularize}:references")
         remove_file("app/models/google_account.rb")
         template("google_account.rb", "app/models/google_account.rb")
         insert_into_file("app/models/#{user_model.underscore.singularize}.rb", "\n\s\shas_one :google_account\n\n", after: "ActiveRecord::Base\n")
