445024bf50afdf3c9f69e00bc8735ba671e241ac
Han <hkfortytwo@gmail.com>
2013-05-16 15:30:43 -0700

    fix migration datatyep

--- lib/generators/glass/install/templates/create_glass_accounts.rb
+++ lib/generators/glass/install/templates/create_glass_accounts.rb
@@ -3,7 +3,7 @@ class CreateGlassAccounts < ActiveRecord::Migration
     create_table :glass_accounts do |t|
       t.string :token
       t.string :refresh_token
-      t.expires_at :expires_at
+      t.integer :expires_at
       t.string :email
       t.string :name
       t.references :<%=user_model.underscore.singularize%>, index: true
