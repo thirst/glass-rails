0be722af3e3f25130be2bb90e7cf80aff5164c56
Han <hkfortytwo@gmail.com>
2013-05-16 18:03:55 -0700

    require all han_dle_bar subtemplates

--- lib/glass_rails.rb
+++ lib/glass_rails.rb
@@ -1,4 +1,5 @@
 require "glass"
 require "glass/engine"
 require "glass/han_dle_bars"
+Dir["#{File.expand_path(File.dirname(__FILE__))}/glass/han_dle_bars/*.rb"].each {|f| require f}
 require "glass/client"
