f6dc6bfa71fbbfc8a0de93169d5eee3bb455bb93
Han <hkfortytwo@gmail.com>
2013-05-17 13:44:31 -0700

    template

---
+++ lib/glass/template.rb
@@ -0,0 +1,12 @@
+require 'action_view'
+module Glass
+  class Template < ActionView::Base
+    include Rails.application.routes.url_helpers
+    include ActionView::Helpers::TagHelper
+    attr_accessor :timeline_item, :template, :extension
+    
+    def default_url_options
+      {host: 'yourhost.org'}
+    end
+  end  
+end
--- lib/glass_rails.rb
+++ lib/glass_rails.rb
@@ -1,5 +1,4 @@
 require "glass"
 require "glass/engine"
-require "glass/han_dle_bars"
-Dir["#{File.expand_path(File.dirname(__FILE__))}/glass/han_dle_bars/*.rb"].each {|f| require f}
+require "glass/template"
 require "glass/client"
