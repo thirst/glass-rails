9b93b612b47e926eb0a31d8ec408f2c1d22edb4a
Han <hkfortytwo@gmail.com>
2013-05-16 18:20:45 -0700

    first test

--- spec/models/glass/han_dle_bars_spec.rb
+++ spec/models/glass/han_dle_bars_spec.rb
@@ -4,11 +4,14 @@ require 'spec_helper'
 describe "HanDleBars" do 
   context "HTML" do 
     before :each do 
-      
+      class TimelineObject; attr_accessor :footer;end;
+      t=TimelineObject.new
+      @template = Glass::HanDleBars::Html.new(t)
     end
-    it "test" do 
-      
+    it "should not convert to an empty string on to_s" do 
+      @template.to_s.should_not eq("")
     end
+    
   end
 
 end
