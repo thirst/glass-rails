5f5fb3ac7aba63a11ab1e33e837a7bbfd3fa7874
Han <hkfortytwo@gmail.com>
2013-05-17 13:46:25 -0700

    template

--- lib/glass/template.rb
+++ lib/glass/template.rb
@@ -2,12 +2,12 @@ require "rails/all"
 require 'action_view'
 module Glass
   class Template < ActionView::Base
-    include ::Rails.application.routes.url_helpers
-    include ::ActionView::Helpers::TagHelper
-    attr_accessor :timeline_item, :template, :extension
+    # include ::Rails.application.routes.url_helpers
+    # include ::ActionView::Helpers::TagHelper
+    # attr_accessor :timeline_item, :template, :extension
     
-    def default_url_options
-      {host: 'yourhost.org'}
-    end
+    # def default_url_options
+    #   {host: 'yourhost.org'}
+    # end
   end  
 end
