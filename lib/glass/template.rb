3135eb36e7a1cfc1cdcfe74f4ceeaf327c65a5b3
Han <hkfortytwo@gmail.com>
2013-05-17 13:47:45 -0700

    template

--- lib/glass/template.rb
+++ lib/glass/template.rb
@@ -5,7 +5,10 @@ module Glass
     # include ::Rails.application.routes.url_helpers
     # include ::ActionView::Helpers::TagHelper
     # attr_accessor :timeline_item, :template, :extension
-    
+    def initialize(param, opts)
+      super(param)
+      binding.pry
+    end
     # def default_url_options
     #   {host: 'yourhost.org'}
     # end
