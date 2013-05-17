c4cb6bf399b81b624c1f404234f03ad56952c865
Han <hkfortytwo@gmail.com>
2013-05-17 13:40:34 -0700

    remove han-dle-bars

--- lib/generators/glass/install/templates/initializer.rb
+++ lib/generators/glass/install/templates/initializer.rb
@@ -1,15 +1,6 @@
 Glass.setup do |config|
-  ## to override the han_dle_bars templates, you
-  ## must specify a directory for han_dle_bars by doing 
-  ## something like this
-
-  ## config.han_dle_bars_directory = "app/han_dle_bars/"
-
-
-  ## you can customize the han_dle_bars extensions by modifying
-  ## the following line:
-  config.han_dle_bars_extension = ".han"
   ## you can override the logo here
   ## config.brandname = "examplename"
+  
   ## config.brandname_styles = {color: "#8BCDF8", font_size: "30px"}
 end
