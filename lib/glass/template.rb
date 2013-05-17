5f15bb55770d997bc8bf420b80044142470049e9
Han <hkfortytwo@gmail.com>
2013-05-17 15:37:04 -0700

    template

--- lib/glass/template.rb
+++ lib/glass/template.rb
@@ -6,12 +6,14 @@ module Glass
     # include ::ActionView::Helpers::TagHelper
     # attr_accessor :timeline_item, :template, :extension
     def initialize(opts={})
+      @content = opts[:content]
       if glass_template_path.present? 
         super(Rails.root.join(glass_template_path))
       else
         super(Rails.root.join("app", "views", "glass-templates"))
       end
     end
+
     private
     def glass_template_path
       ::Glass.glass_template_path
