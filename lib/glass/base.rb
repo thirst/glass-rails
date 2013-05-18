9108b461c687ab21d7cf845c1f0fe254861dfd35
Han <hkfortytwo@gmail.com>
2013-05-17 18:40:53 -0700

    inherit

--- lib/glass/base.rb
+++ lib/glass/base.rb
@@ -1,7 +1,14 @@
 require 'active_record'
 module Glass
-  class Base < ::ActiveRecord::Base
-    self.abstract_class = true
+  class TimelineItem < ::ActiveRecord::Base
+
+    self.table_name = :glass_timeline_items
+
+    belongs_to :google_account
+    attr_accessible :display_time,      :glass_content,     :glass_content_type, 
+                    :glass_created_at,  :glass_etag,        :glass_item_id, 
+                    :glass_kind,        :glass_self_link,   :glass_updated_at, 
+                    :is_deleted
     def self.handles_action(*args)
       puts args
     end
