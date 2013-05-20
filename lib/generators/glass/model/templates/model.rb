4dd3e8c60b4c028067125849e0761bd776aeae0f
Han <hkfortytwo@gmail.com>
2013-05-19 21:48:04 -0700

    blah

--- lib/generators/glass/model/templates/model.rb
+++ lib/generators/glass/model/templates/model.rb
@@ -1,7 +1,9 @@
 class Glass::<%= model_name.camelize %> < Glass::TimelineItem
 
-  has_menu_item :custom_action_name, with: :custom_action_handler
-
+  has_menu_item :custom_action_name, display_name: "this is displayed", 
+                                     icon_url: "http://icons.iconarchive.com/icons/enhancedlabs/lha-objects/128/Filetype-URL-icon.png", 
+                                     with: :custom_action_handler
+                                     
   def custom_action_handler
     ## logic for handling custom action
   end
