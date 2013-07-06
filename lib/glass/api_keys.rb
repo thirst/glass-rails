be245ed55dea252f079307cef9b52ebfcb630144
Kurt Nelson <kurtisnelson@gmail.com>
2013-07-06 14:33:46 -0400

    Pull API keys from ENV not YML

--- lib/glass/api_keys.rb
+++ lib/glass/api_keys.rb
@@ -1,29 +1,20 @@
-require "yaml"
 module Glass
-  ## just a small class to organize the api key logic info
-  ## from the yml file in config. 
   class ApiKeys
     class APIKeyConfigurationError < StandardError; end;
-    attr_accessor :client_id, :client_secret, :google_api_keys
+    attr_accessor :client_id, :client_secret
     def initialize
-      self.google_api_keys = load_yaml_file
       load_keys
       self
     end
     private
     def load_keys
-      if google_api_keys["client_id"].nil? or google_api_keys["client_secret"].nil?
-        raise APIKeyConfigurationError
-      else
-        set_client_keys
-      end
-    end
-    def load_yaml_file
-      ::YAML.load(File.read("#{::Rails.root}/config/google-api-keys.yml"))[::Rails.env]
+      raise APIKeyConfigurationError if ENV['GOOGLE_CLIENT_ID'].nil?
+      raise APIKeyConfigurationError if ENV['GOOGLE_CLIENT_SECRET'].nil?
+      set_client_keys
     end
     def set_client_keys
-      self.client_id = google_api_keys["client_id"]
-      self.client_secret = google_api_keys["client_secret"]
+      self.client_id = ENV['GOOGLE_CLIENT_ID']
+      self.client_secret = ENV['GOOGLE_CLIENT_SECRET']
     end
   end
 end
