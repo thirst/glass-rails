7319732156ab2ca9848f1529930b83f12f7f2dd3
Kurt Nelson <kurtisnelson@gmail.com>
2013-09-25 15:59:40 -0400

    Allow pending and confirmed action states

--- lib/glass/menu_item.rb
+++ lib/glass/menu_item.rb
@@ -1,12 +1,15 @@
 module Glass
   class MenuItem
-    attr_accessor :action,        :id,                    :display_name, 
-                  :icon_url,      :remove_when_selected
+    attr_accessor :action,        :id,                    :display_name,
+                  :icon_url,      :remove_when_selected,
+                  :pending_name,  :confirmed_name,
+                  :pending_icon_url, :confirmed_icon_url
 
-                  
-    BUILT_IN_ACTIONS = [:reply,         :reply_all,       :delete, 
-                        :share,         :read_aloud,      :voice_call, 
-                        :navigate,      :toggle_pinned]
+ 
+    BUILT_IN_ACTIONS = [:reply,         :reply_all,       :delete,
+                        :share,         :read_aloud,      :voice_call,
+                        :navigate,      :toggle_pinned,   :open_uri,
+                        :play_video]
 
 
     def self.create(action_sym, args)
@@ -18,7 +21,11 @@ module Glass
       self.action                 = opts[:action] || "CUSTOM"
       self.id                     = opts[:id]
       self.display_name           = opts[:display_name]
+      self.pending_name           = opts[:pending_name]
+      self.confirmed_name         = opts[:confirmed_name]
       self.icon_url               = opts[:icon_url]
+      self.pending_icon_url       = opts[:pending_icon_url]
+      self.confirmed_icon_url     = opts[:confirmed_icon_url]
       self.remove_when_selected   = opts[:remove_when_selected] || false
     end
 
@@ -28,10 +35,18 @@ module Glass
 
     def serialize
       hash = {action: action}
+      values = [{displayName: display_name, iconUrl: icon_url, state: "DEFAULT"}]
+
+      if self.pending_name
+        values << {displayName: pending_name, iconUrl: pending_icon_url, state: "PENDING"}
+      end
+      if self.confirmed_name
+        values << {displayName: confirmed_name, iconUrl: confirmed_icon_url, state: "CONFIRMED"}
+      end
+
       hash.merge!({id: id,
                    removeWhenSelected: remove_when_selected,
-                   values: [{ displayName: display_name, 
-                              iconUrl: icon_url}]}) if action == "CUSTOM"
+                   values: values}) if action == "CUSTOM"
       hash
     end
   end
--- lib/glass/rails/version.rb
+++ lib/glass/rails/version.rb
@@ -1,5 +1,5 @@
 module Glass
   module Rails
-    VERSION="1.0.4"
+    VERSION="1.0.6"
   end
 end
